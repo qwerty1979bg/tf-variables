5a51a2132ac9fe5d609bee60a3bca6ba4cdecdef
Filip Dimitrov <fdimitrov@Filips-MacBook-Pro.local>
2018-09-20 16:56:31 +0300

    variables

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "interactively_enter_a_variable" {}
+variable "variable_from_env" {}
+variable "variable_from_cmd_line" {}
+variable "variable_from_file" {}
+
+variable "variable_with_a_default" {
+  default = "This is a default value"
+}
